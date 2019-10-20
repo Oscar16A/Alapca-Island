3ec333b211e105172b7715c6cfc6ebe340cf62e5
Oscar Amarillas <oscar16amarillas@gmail.com>
2019-10-20 16:58:37 -0700

    main menu scripts

---
+++ Assets/scene_manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_manager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+}
