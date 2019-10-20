47dcf156bb8a47a8b912bc68119189338b37b41b
nuclearMonkeys <lyndond@uci.edu>
2019-10-20 16:57:56 -0700

    Some changes

---
+++ Assets/_Scripts/ImageFade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFade : MonoBehaviour
+{
+    public Image image;
+
+    void Start() 
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeIn() 
+    {
+        for(float i = 0; i <= 1; i += Time.deltaTime) 
+        {
+            image.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut() 
+    {
+        for(float i = 0; i <= 1; i -= Time.deltaTime) 
+        {
+            image.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+}
